5e17a320daba7a5227112894440df56ca74ce472
ezwendel <43583211+ezwendel@users.noreply.github.com>
2023-02-08 22:36:19 -0500

    make tables for families and individuals
    steps 4-6, using prettytable

--- Project2.py
+++ Project2.py
@@ -2,33 +2,7 @@
 # I pledge my honor that I have abided by the Stevens Honor System.
 
 import re
-
-out = open("output.txt", "w")
-
-def printer(line, valid):
-  line = line.strip()
-  out.write("--> " + line)
-  out.write("\n")
-  parts = line.split(" ")
-  yn = ""
-  if (valid):
-    yn = "Y"
-  else:
-    yn = "N"
-
-  if (len(parts) < 2):
-    out.write("<-- " + line + "|" + yn)
-  elif (len(parts) == 2):
-    level = parts[0]
-    tag = parts[1]
-    out.write("<-- " + level + "|" + "|".join([tag, yn]) + "|")
-  else:
-    level = parts[0]
-    tag = parts[1]
-    arg = " ".join(parts[2:])
-    out.write("<-- " + level + "|" + "|".join([tag, yn, arg]))
-  out.write("\n")
-  
+from prettytable import PrettyTable
 
 gedFile = open("gedcomfile.ged")
 lines = gedFile.readlines()
@@ -39,55 +13,128 @@ tags1none = ["BIRT", "DEAT", "MARR", "DIV"]
 tags2 = ["DATE"]
 months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
 
-for line in lines:
+famDict = {}
+indDict = {}
+
+indTable = PrettyTable()
+indTable.field_names = ["Individual Id", "Name", "Sex", "Birthday", "Death Date"]
+# indTable.add_row(['Elijah Wendel', 'M', 'Feb-8-2000', 'N/A'])
+
+# print(indTable)
+
+for idx, line in enumerate(lines):
   line = line.strip()
   parts = line.split(" ")
-  if (len(parts) == 2):
-    tag = parts[1]
-    level = parts[0]
-    if (level == "0" and tag in tags0):
-      printer(line, True)
-    elif (level == "1" and tag in tags1none):
-      printer(line, True)
-    else:
-      printer(line, False)
-  elif (len(parts) < 3):
-    printer(line, False)
-  else:
+
+  if (len(parts) >= 3):  
     if (parts[2] == "INDI"):
-      iidValid = re.search("^@I\d+@$", parts[1])
-      printer(line, bool(parts[0] == "0" and bool(iidValid)))
-    elif (parts[2] == "FAM"):
-      fidValid = re.search("^@F\d+@$", parts[1])
-      printer(line, bool(parts[0] == "0" and bool(fidValid)))
-    else:
-      arg = " ".join(parts[2:])
-      tag = parts[1]
-      level = parts[0]
-      if (level == "0" and tag in tags0):
-        if (tag == "NOTE"):
-          printer(line, True)
-        else:
-          printer(line, False)
-      elif (level == "1" and tag == "SEX"):
-        printer(line, arg == "F" or arg == "M")
-      elif (level == "1" and tag == "NAME"):
-        printer(line, arg != "")
-      elif (level == "1" and tag in tags1indId):
-        iidValid = re.search("^@I\d+@$", arg)
-        printer(line, bool(iidValid))
-      elif (level == "1" and tag in tags1famId):
-        fidValid = re.search("^@F\d+@$", arg)
-        printer(line, bool(fidValid))
-      elif (level == "2" and tag == "DATE"):
-        dateList = arg.split(" ")
-        if (len(dateList) != 3):
-          printer(line, False)
-        else:
-          if (bool(re.search("^\d+$", dateList[0])) and bool(re.search("^\d+$", dateList[2]))):
-            dateNum = int(dateList[0])
-            printer(line, dateNum > 0 and dateNum <= 31 and dateList[1] in months)
+      print("in INDI")
+      num = re.findall(r'\d+', parts[1])
+      if (idx + 1 < len(lines)):
+        nextLine = lines[idx + 1].strip()
+        nextLineParts = nextLine.split(" ")
+        name = ""
+        if (len(nextLineParts) == 4):
+          name = nextLineParts[2] + " " + nextLineParts[3].strip("/")
+        indDict[int(num[0])] = name
+
+        sex = "N/A"
+        birt = "N/A"
+        deat = "N/A"
+
+        for i in range(idx + 2, len(lines)):
+          currLine = lines[i].strip()
+          currLineParts = currLine.split(" ")
+          level = currLineParts[0]
+          tag = currLineParts[1]
+          
+          if (len(currLineParts) >= 3):
+            arg = " ".join(currLineParts[2:]).strip()
+            if (level == "0" and (currLineParts[2] == "INDI") or (currLineParts[2] == "FAM")):
+              break
+            if (level == "1" and tag == "SEX"):
+              sex = arg
+            elif (level == "1" and tag == "NAME"):
+              name = arg
+            elif (level == "1" and tag == "DEAT"):
+              newLine = lines[i + 1].strip()
+              newLineParts = newLine.split(" ")
+              deat = " ".join(newLineParts[2:]).strip()
           else:
-            printer(line, False)
-      else:
-        printer(line, False)
+            if (level == "1" and tag == "BIRT"):
+              newLine = lines[i + 1].strip()
+              newLineParts = newLine.split(" ")
+              birt = " ".join(newLineParts[2:]).strip()
+            
+        indTable.add_row([num[0], name, sex, birt, deat])
+          
+    elif (parts[2] == "FAM"):
+      print("in FAM")
+      num = re.findall(r'\d+', parts[1])
+      if (idx + 2 < len(lines)):
+        husbLine = lines[idx + 1].strip()
+        husbLineParts = husbLine.split(" ")
+        husbId = re.findall(r'\d+', husbLineParts[2])
+
+        wifeLine = lines[idx + 2].strip()
+        wifeLineParts = wifeLine.split(" ")
+        wifeId = re.findall(r'\d+', wifeLineParts[2])
+
+        marr = "N/A"
+        div = "N/A"
+        children = []
+
+        for i in range(idx + 2, len(lines)):
+          currLine = lines[i].strip()
+          currLineParts = currLine.split(" ")
+          level = currLineParts[0]
+          tag = currLineParts[1]
+          
+          if (len(currLineParts) >= 3):
+            arg = " ".join(currLineParts[2:]).strip()
+            if (level == "0" and (currLineParts[2] == "INDI") or (currLineParts[2] == "FAM")):
+              break
+            if (level == "1" and tag == "CHIL"):
+              chilId = re.findall(r'\d+', arg)
+              children.append(int(chilId[0]))
+          else:
+            if (level == "1" and tag == "MARR"):
+              newLine = lines[i + 1].strip()
+              newLineParts = newLine.split(" ")
+              marr = " ".join(newLineParts[2:]).strip()
+            elif (level == "1" and tag == "DIV"):
+              newLine = lines[i + 1].strip()
+              newLineParts = newLine.split(" ")
+              div = " ".join(newLineParts[2:]).strip()
+
+        famDict[int(num[0])] = (int(num[0]), int(husbId[0]), int(wifeId[0]), marr, div, children)
+
+print(indTable)
+
+indIds = list(indDict.keys())
+indIds.sort()
+sortedIndDict = {i: indDict[i] for i in indIds}
+
+familyIds = list(famDict.keys())
+familyIds.sort()
+sortedFamDict = {i: famDict[i] for i in familyIds}
+
+famTable = PrettyTable()
+famTable.field_names = ["Family Id", "Husband", "Wife", "Marriage Date", "Divorce Date", "Children"]
+
+for fam in sortedFamDict.keys():
+  id, husb, wife, marr, div, children = sortedFamDict[fam]
+
+  husb = indDict[husb]
+  wife = indDict[wife]
+  chil = []
+
+  for child in children:
+    chil.append(indDict[child])
+
+  famTable.add_row([id, husb, wife, marr, div, chil])
+
+print(famTable)
+
+# print(sortedIndDict)
+# print(sortedFamDict)
